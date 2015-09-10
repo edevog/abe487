d0307457726c985a484439db0be412b1cea31811
elizabeth de vogelaere <edevog@email.arizona.edu>
2015-09-10 08:59:41 -0700

    testing

---
+++ wk2/foo/ps1-2.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+my $name = "Beth\n";
+
+print $name;
---
+++ wk2/foo/ps1-3.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+my $name = "Beth\n";
+
+
+print "Hello, $name\n \tHow are you?\n";
+print 'Hello,
+	how are you
+';
