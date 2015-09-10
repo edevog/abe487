973d79b7d5cb154f7c6c3f576dc09c3865aaa8df
elizabeth de vogelaere <edevog@email.arizona.edu>
2015-09-10 09:01:15 -0700

    fixing

--- wk2/perlps1/foo/ps1-7.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env perl
-use strict;
-use warnings;
-
-print 'The difference between single quotes and double quotes is that single
-quotes prints the enclosed text exactly as the user types it. With double
-quotes the user is able to utilize escape codes such as \n to produce a new
-line or \t to tab the text. If these escape codes are contained with in
-single quotes the literal slash and letter t or n will be printed.'
