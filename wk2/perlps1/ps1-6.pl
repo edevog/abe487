ac3da579191cdbd8307fa3a36d21576096646c32
elizabeth de vogelaere <edevog@email.arizona.edu>
2015-09-10 09:02:41 -0700

    finally working

---
+++ wk2/perlps1/ps1-6.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+my $name = "Beth\n";
+
+
+print "Hello, $name\n\t How are you?\n7+3\n";
+print 'Hello, $name\n\t How are you?\n 7+3
+';
+print "Hello, $name\n
+	7+3=", 7+3, "\n";
+
