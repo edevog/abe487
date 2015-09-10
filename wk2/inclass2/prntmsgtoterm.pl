e81d40d036f31196f13a8f14ca0919663623023e
elizabeth de vogelaere <edevog@email.arizona.edu>
2015-09-10 08:53:07 -0700

    week 2

---
+++ wk2/inclass2/prntmsgtoterm.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/env perl
+# file: message.pl
+use strict;
+use warnings;
+print "When that Aprill with his shoures soote\n";
+print "The droghte of March ath perced to the roote,\n";
+print "And bathed every veyne in swich licour\n";
+print "Of which vertu engendered is the flour...\n";
