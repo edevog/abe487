8a9e44ef392c417bfce5893e45f535e55b210060
elizabeth de vogelaere <edevog@email.arizona.edu>
2015-11-02 13:22:10 -0700

    week9

---
+++ obese_lean/get_core_and_variable.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use feature 'say';
+use autodie;
+
+my %kegg;
+open my $core_fh, '>','core';
+open my $variable_fh, '>', 'variable';
+
+my $file_ct = @ARGV;
+for my $file (@ARGV) {
+    open my $fh, '<', $file;
+
+    while(my $line = <$fh>) {
+        chomp($line);
+        my ($k, $ct) = split (',',$line);
+        if ($ct >= 50) {
+        $kegg{$k}++;
+        }
+    opel my $variable_fh, '`dd
+    }
+}
+
+for my $k(keys %kegg) {
+    if ($kegg{$k} == $file_ct) {
+        say $core_fh $k;
+    }
+    else {
+        say $variable_fh $k;
+    }
+}
