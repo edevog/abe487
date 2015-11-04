7f897f88113c70f43bbb1d0b07b6c548c24d4690
elizabeth de vogelaere <edevog@email.arizona.edu>
2015-11-04 11:39:12 -0700

    problem 1

---
+++ week10/02-fasta-search.pl
@@ -0,0 +1,78 @@
+#!/usr/bin/env perl
+#02-fasta-search.pl
+use strict;
+use warnings;
+use autodie;
+use Bio::DB::Fasta'get_all_primary_ids';
+use Bio::SeqIO;
+use feature 'say';
+use Getopt::Long;
+use Pod::Usage;
+
+my %opts = get_opts();
+my @args = @ARGV;
+
+if ($opts{'help'} || $opts{'man'}) {
+    pod2usage({
+        -exitval => 0,
+        -verbose => $opts{'man'} ? 2 : 1
+    });
+}
+
+say "OK";
+
+# --------------------------------------------------
+sub get_opts {
+    my %opts;
+    GetOptions(
+        \%opts,
+        'help',
+        'man',
+    ) or pod2usage(2);
+
+    return %opts;
+}
+
+__END__
+
+# --------------------------------------------------
+
+=pod
+
+=head1 NAME
+
+02-fasta-search.pl - problem 2
+
+=head1 SYNOPSIS
+
+Usage:
+      02-fasta-search.pl file.fa pattern
+
+Options:
+
+  --help   Show brief help and exit
+  --man    Show full documentation
+
+=head1 DESCRIPTION
+
+Retrieve all IDs from a FASTA file matching a given pattern using a regular expression match
+
+=head1 SEE ALSO
+
+perl.
+
+=head1 AUTHOR
+
+Beth De Vogelaere E<lt>edevog@email.arizona.eduE<gt>.
+
+=head1 COPYRIGHT
+
+Copyright (c) 2015 Beth De Vogelaere
+
+This module is free software; you can redistribute it and/or
+modify it under the terms of the GPL (either version 1, or at
+your option, any later version) or the Artistic License 2.0.
+Refer to LICENSE for the full license text and to DISCLAIMER for
+additional warranty disclaimers.
+
+=cut
